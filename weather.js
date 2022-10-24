f4ef6a8c5140e5d7fcc6e28b3db9397b90940f49
mohamadAlhawameda <alhawameda4@gmail.com>
2022-10-24 02:06:36 -0400

    first commit

---
+++ weather.js
@@ -0,0 +1,31 @@
+function MyWeahter() {
+    let sel = document.getElementById("Menu").value;
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + sel + "&units=metric&appid=86de1d4ee4dd12e3bcfe36542183215a")
+        .then(response => response.json())
+        .then(data => MyJson(data))
+        .catch(message => alert(message));;
+}
+
+function UsingXHR(url) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.responseType = "json";
+    xhttp.onreadystatechange = function () {
+
+    };
+    xhttp.open("GET", url, true);
+    xhttp.get();
+}
+
+function MyJson(json) {
+    document.getElementById("city").innerHTML = json.name;
+
+    document.getElementById("temp").innerHTML = "Tempature : " + json.main.temp + "°";
+    document.getElementById("feels").innerHTML = "Feels like : " + json.main.feels_like + "°";
+    document.getElementById("wind").innerHTML = "Wind speed : " + json.wind.speed;
+    document.getElementById("hum").innerHTML = "Humidity: " + json.main.humidity
+    document.getElementById("cuName").innerHTML = "Country : " + json.sys.country;
+
+
+
+
+}
